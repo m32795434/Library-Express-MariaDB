d352596271de9394f188f2aa7de06472e1d1da4a
m32795434 <manubravard@gmail.com>
2021-06-29 21:00:23 -0300

    creamos rutas, modificamos estilos h1, falta estilos nav

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'gatitos' });
+});
+
+router.get ('/contacto', (req, res) => {
+  res.render('pages/contacto');
+});
+router.get ('/nosotros', (req, res) => {
+  res.render('pages/nosotros');
 });
 
 module.exports = router;
