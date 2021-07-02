8a07451e560852c9dd98dac27cf346eb91adf138
m32795434 <manubravard@gmail.com>
2021-07-01 22:35:25 -0300

    query a db autor y libro

--- api/index.js
+++ api/index.js
@@ -2,14 +2,24 @@
 const db = require('../models');
 
 const getBooks = async () => {
-    const books = await db.libro.findAll()
+    const books = await db.libro.findAll({include: db.autor.id})//exporto solo adicional el "id del autor", no todos los datos
         .then(result => {
             return result;
         });
 
     return books;
 }
+const getAutores = async () => {
+    const autores = await db.autor.findAll({include: db.libro})
+         .then(result => {
+             return result;
+         });
+
+    return autores;
+}
 
 module.exports = {
-    getBooks
+    getBooks, getAutores
 }
+
+
--- routes/index.js
+++ routes/index.js
@@ -5,16 +5,21 @@ const api = require('../api');//como es index.js a quien busco, no necesito espe
 /* GET home page. */
 router.get('/', async (req, res,) =>{
   const books = await api.getBooks();
-  console.log(books);
   //res.render('index', { title: 'gatitos' });
-  res.send(books);
+  res.send(books); //muestro los books en navegador
+});
+router.get('/autores', async (req, res,) =>{
+  const autores = await api.getAutores();
+  res.send(autores);
 });
 
 router.get ('/contacto', (req, res) => {
   res.render('pages/contacto');
 });
+
 router.get ('/nosotros', (req, res) => {
   res.render('pages/nosotros');
 });
+
 
 module.exports = router;
